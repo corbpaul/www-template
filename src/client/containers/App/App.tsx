8398b231a17596b133993fcf4c604c176f642fda
Paul Corbett <3407807+corbpaul@users.noreply.github.com>
2021-06-17 15:05:58 +1000

    chore: styled-component config (#23)

--- src/client/containers/App/App.tsx
+++ src/client/containers/App/App.tsx
@@ -1,9 +1,17 @@
-import { Route, Switch } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import { Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import React from 'react';
+import styled from 'styled-components';
+
+import { GlobalStyles, themes } from '../../styles/';
+
+const StyledHeading = styled.h1`
+  color: ${({ theme }: { theme: App.Theme }) => theme.colors.primary};
+`;
 
 function Home() {
-  return <div>Home Page</div>;
+  return <StyledHeading>Home Page</StyledHeading>;
 }
 
 function App() {
@@ -13,9 +21,12 @@ function App() {
         <title>My Title</title>
         <meta name="description" content="Helmet application" />
       </Helmet>
-      <Switch>
-        <Route exact={true} path="/" component={Home} />
-      </Switch>
+      <ThemeProvider theme={themes.base}>
+        <GlobalStyles />
+        <Switch>
+          <Route exact={true} path="/" component={Home} />
+        </Switch>
+      </ThemeProvider>
     </>
   );
 }
