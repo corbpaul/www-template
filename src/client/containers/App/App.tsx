14c4727cc58b2aac058313da23bfa8111e4ce347
Paul Corbett <3407807+corbpaul@users.noreply.github.com>
2021-06-17 14:06:54 +1000

    chore: React server/client setup (#18)

---
+++ src/client/containers/App/App.tsx
@@ -0,0 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import React from 'react';
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function App() {
+  return (
+    <>
+      <Helmet>
+        <title>My Title</title>
+        <meta name="description" content="Helmet application" />
+      </Helmet>
+      <Switch>
+        <Route exact={true} path="/" component={Home} />
+      </Switch>
+    </>
+  );
+}
+
+export { App };
