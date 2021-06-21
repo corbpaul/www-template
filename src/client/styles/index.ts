4186848909cf3b1a1abb52d1d86410695d7c58c5
Paul Corbett <3407807+corbpaul@users.noreply.github.com>
2021-06-21 15:58:26 +1000

    chore: add basic typography - font-family (#40)

--- src/client/styles/index.ts
+++ src/client/styles/index.ts
@@ -5,6 +5,10 @@ import { themes } from './themes';
 
 const GlobalStyles = createGlobalStyle`
  ${normalize}
+
+ body {
+   font-family: ${({ theme }: { theme: App.Theme }) => theme.fonts.body};
+ }
 `;
 
 export { GlobalStyles, themes };
